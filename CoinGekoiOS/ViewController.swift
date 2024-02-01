0d1f1381a7c1aa81037c8dbf5167824b91212c73
barufe <belkisarufe@gmail.com>
2024-01-31 20:33:56 -0500

    Initial Commit

---
+++ CoinGekoiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinGekoiOS
+//
+//  Created by Germain Seijas on 31/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
